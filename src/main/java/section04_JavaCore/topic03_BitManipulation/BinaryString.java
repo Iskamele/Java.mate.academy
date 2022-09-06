c36e06b3f44c96a05016ebd0f43d785b9a31ec58
Iskamele <iskamele@gmail.com>
2022-09-06 14:57:10 +0300

    practice

---
+++ src/main/java/section04_JavaCore/topic03_BitManipulation/BinaryString.java
@@ -0,0 +1,23 @@
+package section04_JavaCore.topic03_BitManipulation;
+
+public class BinaryString {
+    public static void main(String[] args) {
+        System.out.println(toBinaryString(0));
+        System.out.println(toBinaryString(1));
+        System.out.println(toBinaryString(2));
+        System.out.println(toBinaryString(3));
+        System.out.println(toBinaryString(4));
+        System.out.println(toBinaryString(5));
+    }
+
+    private static String toBinaryString(int value) {
+        StringBuilder builder = new StringBuilder();
+        int countFor = value;
+
+        for (int i = 0; i < countFor && value != 0; i++) {
+            builder.insert(0,value % 2);
+            value /= 2;
+        }
+        return countFor != 0 ? builder.toString() : "0";
+    }
+}
---
+++ src/main/java/section04_JavaCore/topic03_BitManipulation/DoubledNumber.java
@@ -0,0 +1,13 @@
+package section04_JavaCore.topic03_BitManipulation;
+
+public class DoubledNumber {
+    public static void main(String[] args) {
+        System.out.println(getDoubledNumber(-2));
+        System.out.println(getDoubledNumber(3));
+        System.out.println(getDoubledNumber(10));
+    }
+
+    public static int getDoubledNumber(int number) {
+        return number << 1;
+    }
+}
