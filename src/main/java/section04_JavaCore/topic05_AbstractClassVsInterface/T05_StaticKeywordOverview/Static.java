3b28a2e6b60684235b7fc8616673d3db9b23b77e
Iskamele <iskamele@gmail.com>
2022-11-04 12:47:20 +0200

    Static keyword overview

---
+++ src/main/java/section04_JavaCore/topic05_AbstractClassVsInterface/T05_StaticKeywordOverview/Static.java
@@ -0,0 +1,19 @@
+package section04_JavaCore.topic05_AbstractClassVsInterface.T05_StaticKeywordOverview;
+
+public class Static {
+    public static void main(String[] args) {
+        // static keyword
+        sayHello();
+
+        Static aStatic = new Static();
+        aStatic.sayNonStatic();
+    }
+    public static void sayHello(){
+        System.out.println("Hello mates!");
+    }
+
+    public void sayNonStatic(){
+        System.out.println("I am not static");
+        sayHello();
+    }
+}
---
+++ src/main/java/section04_JavaCore/topic05_AbstractClassVsInterface/T05_StaticKeywordOverview/User.java
@@ -0,0 +1,21 @@
+package section04_JavaCore.topic05_AbstractClassVsInterface.T05_StaticKeywordOverview;
+
+public class User {
+    // Статичное поле не описывает состояние объекта, оно описывает состояние класса
+    public static String name;
+    public int age;
+
+    /*
+    public static void accessToFields() {
+        name = "Bob";
+        age = 12; //Non-static field 'age' cannot be referenced from a static context
+    }
+     */
+
+    @Override
+    public String toString() {
+        age = 12;
+        name = "John";
+        return "name is " + name;
+    }
+}
