afa9ff9de5a9dbb3291c850d32f71d8e3d03c62a
Iskamele <iskamele@gmail.com>
2022-11-04 13:32:28 +0200

    Static and non-static classes

---
+++ src/main/java/section04_JavaCore/topic05_AbstractClassVsInterface/T07_StaticAndNonStaticClasses/Car.java
@@ -0,0 +1,11 @@
+package section04_JavaCore.topic05_AbstractClassVsInterface.T07_StaticAndNonStaticClasses;
+
+public class Car {
+    static class Builder {
+
+    }
+
+    class Inner {
+
+    }
+}
