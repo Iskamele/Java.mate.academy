b6c475c9f75f7ea60dea7fe1f979d93eb723be3e
Iskamele <iskamele@gmail.com>
2022-11-04 12:38:05 +0200

    Interface extending

---
+++ src/main/java/section04_JavaCore/topic05_AbstractClassVsInterface/T04_InterfaceExtending/Drivable.java
@@ -0,0 +1,5 @@
+package section04_JavaCore.topic05_AbstractClassVsInterface.T04_InterfaceExtending;
+
+public interface Drivable {
+    void drive();
+}
---
+++ src/main/java/section04_JavaCore/topic05_AbstractClassVsInterface/T04_InterfaceExtending/Ford.java
@@ -0,0 +1,13 @@
+package section04_JavaCore.topic05_AbstractClassVsInterface.T04_InterfaceExtending;
+
+public class Ford implements Vehicle{
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void startEngine() {
+
+    }
+}
