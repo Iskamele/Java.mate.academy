243a6a2e415a923dca1dac13c90f16096618c3a7
Iskamele <iskamele@gmail.com>
2022-11-04 13:40:42 +0200

    Constants

---
+++ src/main/java/section04_JavaCore/topic05_AbstractClassVsInterface/T09_Constants/Main.java
@@ -0,0 +1,10 @@
+package section04_JavaCore.topic05_AbstractClassVsInterface.T09_Constants;
+
+public class Main {
+    private static final int MAX_AGE = 65;
+
+    public static void main(String[] args) {
+        // constants
+        final String text = "Text";
+    }
+}
---
+++ src/main/java/section04_JavaCore/topic05_AbstractClassVsInterface/T09_Constants/User.java
@@ -0,0 +1,5 @@
+package section04_JavaCore.topic05_AbstractClassVsInterface.T09_Constants;
+
+public class User {
+    private int age;
+}
