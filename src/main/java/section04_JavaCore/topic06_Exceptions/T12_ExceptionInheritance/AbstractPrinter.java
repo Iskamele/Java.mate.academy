7a794dabf2403baf12c53fe94486993d45d9a616
Iskamele <iskamele@gmail.com>
2022-11-09 17:04:28 +0200

    Exception inheritance

---
+++ src/main/java/section04_JavaCore/topic06_Exceptions/T12_ExceptionInheritance/AbstractPrinter.java
@@ -0,0 +1,7 @@
+package section04_JavaCore.topic06_Exceptions.T12_ExceptionInheritance;
+
+import java.io.IOException;
+
+public abstract class AbstractPrinter {
+    public abstract void print() throws IOException;
+}
