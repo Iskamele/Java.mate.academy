52dc433c7069646c02fb97fbc95206d65b101689
Iskamele <iskamele@gmail.com>
2022-11-11 22:44:07 +0200

    practice

---
+++ src/main/java/section04_JavaCore/topic04_OOP/practice/RobotRoute/Direction.java
@@ -0,0 +1,8 @@
+package section04_JavaCore.topic04_OOP.practice.RobotRoute;
+
+public enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
---
+++ src/main/java/section04_JavaCore/topic04_OOP/practice/RobotRoute/RobotRoute.java
@@ -0,0 +1,48 @@
+package section04_JavaCore.topic04_OOP.practice.RobotRoute;
+
+public class RobotRoute {
+    public void moveRobot(Robot robot, int toX, int toY) {
+        if (toX > robot.getX()) {
+            getTurn(robot, Direction.RIGHT);
+            getMove(robot, toX, robot.getX());
+        }
+        if (toY > robot.getY()) {
+            getTurn(robot, Direction.UP);
+            getMove(robot, toY, robot.getY());
+        }
+        if (toX < robot.getX()) {
+            getTurn(robot, Direction.LEFT);
+            getMove(robot, toX, robot.getX());
+        }
+        if (toY < robot.getY()) {
+            getTurn(robot, Direction.DOWN);
+            getMove(robot, toY, robot.getY());
+        }
+    }
+
+    private void getMove(Robot robot, int to, int from) {
+        while (to != from) {
+            robot.stepForward();
+            if (from < to) {
+                from++;
+            } else {
+                from--;
+            }
+        }
+    }
+
+    private void getTurn(Robot robot, Direction direction) {
+        if (robot.getDirection() == Direction.UP & direction == Direction.RIGHT
+                || robot.getDirection() == Direction.RIGHT & direction == Direction.DOWN
+                || robot.getDirection() == Direction.DOWN & direction == Direction.LEFT
+                || robot.getDirection() == Direction.LEFT & direction == Direction.UP) {
+            while (robot.getDirection() != direction) {
+                robot.turnRight();
+            }
+        } else {
+            while (robot.getDirection() != direction) {
+                robot.turnLeft();
+            }
+        }
+    }
+}
