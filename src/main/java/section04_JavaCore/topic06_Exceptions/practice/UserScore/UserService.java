9f4821bf51920c4b10c969defd35d65e5b4b5771
Iskamele <iskamele@gmail.com>
2022-12-02 14:39:12 +0200

    Practice

--- src/main/java/section04_JavaCore/topic06_Exceptions/practice/UserScore/UserService.java
+++ src/main/java/section04_JavaCore/topic06_Exceptions/practice/UserScore/UserService.java
@@ -6,18 +6,6 @@ import java.util.regex.Pattern;
 public class UserService {
     private static final Pattern intsOnly = Pattern.compile("\\d+");
 
-//    private void test(){
-//        String test = "second-user@gmail.com:20";
-//        Pattern intsOnly = Pattern.compile("\\d+");
-//        Matcher makeMatch = intsOnly.matcher(test);
-//        makeMatch.find();
-//        int answer = Integer.parseInt(makeMatch.group());
-//        System.out.println(answer);
-//    }
-
-
-    // https://www.techiedelight.com/ru/find-occurrences-of-substring-string-java/
-    // https://javarush.ru/groups/posts/isklyucheniya-java
     public int getUserScore(String[] records, String email) {
         int answer = 0;
         if (records.length == 0) {
@@ -30,8 +18,6 @@ public class UserService {
                 answer = Integer.parseInt(makeMatch.group());
                 return answer;
             }
-
-
         }
         throw new UserNotFoundException("User with given email doesn't exist");
     }
