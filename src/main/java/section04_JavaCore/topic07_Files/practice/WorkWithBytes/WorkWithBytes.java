50fc28b1a04b71c8fcf307aed4a5c3de407e73a3
Iskamele <iskamele@gmail.com>
2022-12-05 13:20:24 +0200

    We will practice with writing array of bytes to the file. Let's implement method: writeBytesToFile(String fileName, byte[] data) with two parameters:
    String fileName - you should create file with this name and write array of bytes there.
    byte[] data - your data.
    Don't convert bytes to String! Let's write byte[] directly to file.

---
+++ src/main/java/section04_JavaCore/topic07_Files/practice/WorkWithBytes/WorkWithBytes.java
@@ -0,0 +1,22 @@
+package section04_JavaCore.topic07_Files.practice.WorkWithBytes;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
+public class WorkWithBytes {
+  public void writeBytesToFile(String fileName, byte[] data) {
+    File file = new File(fileName);
+    try {
+      file.createNewFile();
+    } catch (IOException e) {
+      throw new RuntimeException("Can't create file", e);
+    }
+    try {
+      Files.write(file.toPath(), data, StandardOpenOption.APPEND);
+    } catch (IOException e) {
+      throw new RuntimeException("Can't write data to file", e);
+    }
+  }
+}
