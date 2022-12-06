acc03baff9ccac0eae4c096494fbeb18de078c04
Iskamele <iskamele@gmail.com>
2022-12-06 22:02:58 +0200

    Builder What is builder The builder creational pattern, is an alternative way to construct complex objects. It should be used when you want to build different objects using same object building process. Builder pattern also helps minimizing the number of parameters in constructor and thus there is no need to pass in null for optional parameters to the constructor. Certainly, the number of lines of code increase at least to double in builder pattern, but the effort pays off in terms of design flexibility and much more readable code. The parameters to the constructor are reduced and are provided in highly readable method calls.
    Let’s see how we can implement builder design pattern in java:
    
    First of all you need to create a static nested class and then copy all the arguments from the outer class to the Builder class.
    Builder class should have a public constructor. It can be empty or with all the required attributes as parameters.
    Builder class should have methods to set the optional parameters and it should return the same Builder object after setting the optional attribute.
    The final step is to provide a build() method in the builder class that will return the Object needed by client program. For this we need to have a private constructor in the outer class with Builder class as argument.

---
+++ src/main/java/section04_JavaCore/topic09_PatternsAndRecurstion/T05_BuilderUsageExample/Country.java
@@ -0,0 +1,49 @@
+package section04_JavaCore.topic09_PatternsAndRecurstion.T05_BuilderUsageExample;
+
+public class Country {
+    private String name;
+    private int population;
+    private Country[] neighbours;
+    private String[] regions;
+    private String[] previousNames;
+
+    private Country(Builder builder) {
+        this.name = builder.name;
+        this.population = builder.population;
+        this.neighbours = builder.neighbours;
+        this.regions = builder.regions;
+        this.previousNames = builder.previousNames;
+    }
+
+    public static class Builder {
+        private String name;
+        private int population;
+        private Country[] neighbours;
+        private String[] regions;
+        private String[] previousNames;
+
+        public Builder(String name, int population) {
+            this.name = name;
+            this.population = population;
+        }
+
+        public Builder setNeighbours(Country[] neighbours) {
+            this.neighbours = neighbours;
+            return this;
+        }
+
+        public Builder setRegions(String[] regions) {
+            this.regions = regions;
+            return this;
+        }
+
+        public Builder setPreviousNames(String[] previousNames) {
+            this.previousNames = previousNames;
+            return this;
+        }
+
+        public Country build() {
+            return new Country(this);
+        }
+    }
+}
---
+++ src/main/java/section04_JavaCore/topic09_PatternsAndRecurstion/T05_BuilderUsageExample/CountryExample.java
@@ -0,0 +1,17 @@
+package section04_JavaCore.topic09_PatternsAndRecurstion.T05_BuilderUsageExample;
+
+public class CountryExample {
+    public static void main(String[] args) {
+        // Builder example
+        Country vatican = new Country.Builder("Vatican", 1000).build();
+        Country ukraine = new Country.Builder("Ukraine", 36_000_000)
+                .setRegions(new String[] {"Kyiv", "Donetsk", "Simferopol"})
+                .setPreviousNames(new String[] {"Kyiv Rus'"})
+                .setNeighbours(new Country[] { /* TODO add countries*/})
+                .build();
+
+        Country australia = new Country.Builder("Australia", 50_000_000)
+                .setRegions(new String[]{"Victoria"})
+                .build();
+    }
+}
