e0d6af00e67223514671ff86563b7c15e8a2adb4
Iskamele <iskamele@gmail.com>
2022-12-06 21:25:44 +0200

    Static factory method Static factory method is a creational pattern, alternative to constructor for initialization of an object.
    It's a public static method in the object that returns a new instance of the object. In most cases this method is called of() but you can call it as you need depending on your use case. Below you can see an example of such method. In our case it's called of() and with its help we can initialize our object called User, passing his name and age.
    
    Example
    public class Main {
        public static void main(String[] args) {
            User user = User.of("John", 18);
        }
    }
    
    public class User {
      private String name;
      private int age;
    
      private User(String name, int age) {
        this.name = name;
        this.age = age;
      }
    
      public static User of(String name, int age) {
        return new User(name, age);
      }
    
      ....
    
    Advantages and Disadvantages of static factory method compared with constructor
    Advantages
    Static factory method can have a custom name
    It can return an object of any class subtype as its return type
    Inside the static factory method we can perform any activity like increasing count value for every object creation.
    Disadvantages
    Classes where you use only static factory methods for initialization and without public or protected constructors cannot be subclassed.
    They may be not obvious to use. They don't stand out in API documentation in the way that constructors do, so it can be difficult to figure out how to instantiate a class that provides static factory methods instead of constructors.

---
+++ src/main/java/section04_JavaCore/topic09_PatternsAndRecurstion/T03_StaticFabricMethod/Main.java
@@ -0,0 +1,19 @@
+package section04_JavaCore.topic09_PatternsAndRecurstion.T03_StaticFabricMethod;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        User bob = User.of("Bob", 22);
+        User alice = User.of("Alice", 19);
+
+        System.out.println(bob);
+        System.out.println(alice);
+
+        // examples
+        Integer value = Integer.valueOf("123");
+        String strValue = String.valueOf(123);
+        List<Integer> numbers = List.of(1, 2, 3, 4);
+        System.out.println(numbers);
+    }
+}
