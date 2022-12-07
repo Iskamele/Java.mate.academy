ddc534be613db344acdd9b296e0a7f086ded7990
Iskamele <iskamele@gmail.com>
2022-12-07 11:40:11 +0200

    Implement method getFactorial(int number) which will return factorial of the given number. Solve this task using recursion.
    Example input: 6
    
    Output input: 720

---
+++ src/main/java/section04_JavaCore/topic09_PatternsAndRecurstion/practice/Factorial/FibonacciSequence.java
@@ -0,0 +1,8 @@
+package section04_JavaCore.topic09_PatternsAndRecurstion.practice.Factorial;
+
+public class FibonacciSequence {
+  public int getFibonacciNumber(int n) {
+    //write your code here
+    return 0;
+  }
+}
