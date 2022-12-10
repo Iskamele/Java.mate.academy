97e90bb5745f91b29f496b783091ab600510796e
Iskamele <iskamele@gmail.com>
2022-12-10 10:39:37 +0200

    Generics Pair practice fix and comments

--- src/main/java/section04_JavaCore/topic10_Generics/practice/Pair/Pair.java
+++ src/main/java/section04_JavaCore/topic10_Generics/practice/Pair/Pair.java
@@ -4,6 +4,8 @@ import java.util.Objects;
 
 @SuppressWarnings("unchecked") // do not remove this line
 public class Pair<I, S> {
+    // fields must be private final
+    // field names in this task must match getter methods
     I integerElement;
     S stringElement;
 
@@ -33,21 +35,22 @@ public class Pair<I, S> {
         if (pair == this) {
             return true;
         }
+        // this and the following comparison can be combined
         if (pair == null) {
             return false;
         }
         if (getClass() != pair.getClass()) {
             return false;
         }
-        return (Objects.equals(integerElement, ((Pair<?, ?>) pair).integerElement))
-                && (Objects.equals(stringElement, ((Pair<?, ?>) pair).stringElement));
+        // Mentor cast Object to Pair: Pair<T, K> pair = (Pair<T, K>) obj;
+        // And returned a comparison of objects using getters, checking them for null
         /*
-        return (Objects.equals(integerElement, ((Pair) pair).integerElement))
-                && (Objects.equals(stringElement, ((Pair) pair).stringElement));
+        return (pair.getKey() == key
+                || (pair.getKey() != null && pair.getKey().equals(key)))
+                && (pair.getValue() == value
+                || (pair.getValue() != null && pair.getValue().equals(value)));
          */
-        /*
-        return (integerElement == ((Pair) pair).integerElement || integerElement != null && integerElement.equals(((Pair) pair).integerElement))
-                && (stringElement == ((Pair) pair).stringElement || stringElement != null && stringElement.equals(((Pair) pair).stringElement));
-        */
+        return (Objects.equals(integerElement, ((Pair<I, S>) pair).integerElement))
+                && (Objects.equals(stringElement, ((Pair<I, S>) pair).stringElement));
     }
 }
