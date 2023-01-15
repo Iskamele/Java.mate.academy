c36faeb91843ddd1db96c655ce35d0ee5ed2776b
Iskamele <iskamele@gmail.com>
2023-01-15 03:46:50 +0200

    HashMap another students work

---
+++ src/main/java/section04_JavaCore/topic14_HashMap/practice/OtherStudents/viktoriiaMaherko/MyMap.java
@@ -0,0 +1,9 @@
+package section04_JavaCore.topic14_HashMap.practice.OtherStudents.viktoriiaMaherko;
+
+public interface MyMap<K, V> {
+    void put(K key, V value);
+
+    V getValue(K key);
+
+    int getSize();
+}
