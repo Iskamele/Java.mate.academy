a530d09e849b23070da6dc1d377b76047c3eae41
Iskamele <iskamele@gmail.com>
2023-01-17 16:46:31 +0200

    set theory

---
+++ src/main/java/section04_JavaCore/topic17_SetQueueStackAndComparator/theory/T03_ComparatorVsComparable/Main.java
@@ -0,0 +1,29 @@
+package section04_JavaCore.topic17_SetQueueStackAndComparator.theory.T03_ComparatorVsComparable;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TreeSet
+
+        Set<Integer> numbers = new TreeSet<>();
+        numbers.add(10);
+        numbers.add(-10);
+        numbers.add(-50);
+        numbers.add(0);
+        numbers.add(99);
+        numbers.add(7);
+
+        System.out.println(numbers);
+
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Bob", 23));
+        users.add(new User("Alice", 22));
+        users.add(new User("John", 27));
+        users.add(new User("Bob", 27));
+        users.add(new User("Bob", 19));
+        System.out.println(users);
+    }
+}
---
+++ src/main/java/section04_JavaCore/topic17_SetQueueStackAndComparator/theory/T03_ComparatorVsComparable/User.java
@@ -0,0 +1,59 @@
+package section04_JavaCore.topic17_SetQueueStackAndComparator.theory.T03_ComparatorVsComparable;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(User user) {
+        int namesCompared = name.compareTo(user.name);
+        if (namesCompared!=0){
+            return namesCompared;
+        }
+        return Integer.compare(age, user.age);
+    }
+}
