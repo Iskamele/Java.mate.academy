130825679270afe115aeef0575d1ad65bb05ced5
Iskamele <iskamele@gmail.com>
2023-01-20 21:33:40 +0200

    comparator test

---
+++ src/main/java/section04_JavaCore/topic18_JavaJUnit/theory/T01_WhatIsTesting/Main.java
@@ -0,0 +1,37 @@
+package section04_JavaCore.topic18_JavaJUnit.theory.T01_WhatIsTesting;
+
+public class Main {
+        // Testing intro
+
+        /*
+        1. What is Testing?
+        2. What are the benefits of tests?
+        3. What are the types of tests?
+
+        Testing is a test of the correspondence between the actual
+        and expected behavior of a program, carried out on a concrete
+        set of tests chosen in a certain way. Simple put — testing
+        is checking whether everything is working as expected in your program.
+
+        Types of Testing:
+        1. UNIT TESTING
+        2. INTEGRATION TESTING
+        3. REGRESSION TESTING
+        4. SMOKE TESTING
+        5. ALPHA TESTING
+        6. BETA TESTING
+        7. SYSTEM TESTING
+        8. STRESS TESTING
+        9. PERFORMANCE TESTING
+        AND MANY OTHERS
+
+        As developers, we should write unit tests for our code.
+        It's a way of testing a unit - the smallest piece of code
+        that can be logically isolated in a system. In most programming
+        languages, the unit is a function (or a method in Java) or class
+        or a property. Again, it is important that the unit is isolated
+        for the testing to be done independently.
+
+        One of the most popular testing frameworks for Java is JUnit.
+         */
+}
