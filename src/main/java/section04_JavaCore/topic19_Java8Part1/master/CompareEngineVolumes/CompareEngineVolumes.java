413c7946ba3d4d164d47065af7bfce59a187ab6a
Iskamele <iskamele@gmail.com>
2023-01-29 18:28:58 +0200

    remove

--- src/main/java/section04_JavaCore/topic19_Java8Part1/master/CompareEngineVolumes/CompareEngineVolumes.java
+++
@@ -1,5 +0,0 @@
-package section04_JavaCore.topic19_Java8Part1.master.CompareEngineVolumes;
-
-public class CompareEngineVolumes {
-  //write your code here
-}
--- src/main/java/section04_JavaCore/topic19_Java8Part1/practice/CompareEngineVolumes/CompareEngineVolumes.java
+++
@@ -1,7 +0,0 @@
-package section04_JavaCore.topic19_Java8Part1.practice.CompareEngineVolumes;
-
-import java.util.function.ToIntBiFunction;
-
-public class CompareEngineVolumes {
-    ToIntBiFunction compareEngineVolumes = (o, o2) -> Integer.parseInt(o.toString()) - Integer.parseInt(o2.toString());
-}
