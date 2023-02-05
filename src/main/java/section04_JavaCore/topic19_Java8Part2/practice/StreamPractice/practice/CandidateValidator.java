50dd275b2b3bd76a4f9061446f864d2a0a2cb6e1
Iskamele <iskamele@gmail.com>
2023-02-05 19:46:58 +0200

    Stream Practice task

---
+++ src/main/java/section04_JavaCore/topic19_Java8Part2/practice/StreamPractice/practice/CandidateValidator.java
@@ -0,0 +1,29 @@
+package section04_JavaCore.topic19_Java8Part2.practice.StreamPractice.practice;
+
+
+import java.util.function.Predicate;
+import section04_JavaCore.topic19_Java8Part2.practice.StreamPractice.model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String VALID_NATIONALITY = "Ukrainian";
+    private static final String RESIDENCE_DATE_SPLITTER = "-";
+    private static final int POSITION_OF_START_OF_LIVING_PERIOD = 0;
+    private static final int POSITION_OF_END_OF_LIVING_RESIDENCE = 1;
+    private static final int MIN_PERIOD_IN_COUNTRY = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(VALID_NATIONALITY)
+                && candidate.isAllowedToVote()
+                && isValidPeriodOfResidence(candidate);
+    }
+
+    private boolean isValidPeriodOfResidence(Candidate candidate) {
+        String[] dates = candidate.getPeriodsInUkr().split(RESIDENCE_DATE_SPLITTER);
+        int startDate = Integer.parseInt(dates[POSITION_OF_START_OF_LIVING_PERIOD]);
+        int endDate = Integer.parseInt(dates[POSITION_OF_END_OF_LIVING_RESIDENCE]);
+        return endDate - startDate >= MIN_PERIOD_IN_COUNTRY;
+    }
+}
