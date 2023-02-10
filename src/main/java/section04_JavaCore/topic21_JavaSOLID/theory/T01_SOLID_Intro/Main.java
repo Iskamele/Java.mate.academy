2d72e4a02ac8fd51b94bf6d7c876d76a26eb61ec
Iskamele <iskamele@gmail.com>
2023-02-10 21:41:10 +0200

    SOLID Intro, theory

---
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/theory/T01_SOLID_Intro/Main.java
@@ -0,0 +1,13 @@
+package section04_JavaCore.topic21_JavaSOLID.theory.T01_SOLID_Intro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // SOLID
+        // SRP - Single Responsibility Principle
+        // OCP - Open Closed Principle
+        // LSP - Liskov Substitution Principle
+        // ISP - Interface Segregation Principle
+        // DI - Dependency Inversion Principle
+    }
+}
