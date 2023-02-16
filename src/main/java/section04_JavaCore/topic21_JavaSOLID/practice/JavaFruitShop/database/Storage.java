6771b276d70578cc58aeddaba04a1cf3490c3343
Iskamele <iskamele@gmail.com>
2023-02-16 18:35:18 +0200

    Added Map Storage for shop fruits.

---
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/database/Storage.java
@@ -0,0 +1,8 @@
+package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Storage {
+    public static Map<String, Integer> fruitStorage = new HashMap<>();
+}
