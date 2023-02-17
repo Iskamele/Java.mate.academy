10b486bccda44a1ef1162c1576d273b2636c88f5
Iskamele <iskamele@gmail.com>
2023-02-17 13:01:44 +0200

    Added new exception

---
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/exception/FileException.java
@@ -0,0 +1,7 @@
+package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.exception;
+
+public class FileException extends RuntimeException {
+    public FileException(String message) {
+        super(message);
+    }
+}
