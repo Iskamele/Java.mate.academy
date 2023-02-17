e46d39ed2e6f105a802281f042c51de1dad392b5
Iskamele <iskamele@gmail.com>
2023-02-17 13:02:13 +0200

    Added new get method

--- src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/operationImpl/BalanceOperation.java
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/operationImpl/BalanceOperation.java
@@ -2,12 +2,11 @@ package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.operationImp
 
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.database.Storage;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.model.FruitTransaction;
-import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.operationStrategy.OperationHandler;
 
 public class BalanceOperation implements OperationHandler {
 
     @Override
     public void handleOperation(FruitTransaction transaction) {
-        Storage.fruitStorage.put(transaction.getFruit(), transaction.getQuantity());
+        Storage.getFruitStorage().put(transaction.getFruit(), transaction.getQuantity());
     }
 }
