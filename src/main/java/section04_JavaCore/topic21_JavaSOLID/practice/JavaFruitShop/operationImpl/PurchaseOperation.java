cd6e1e4143a16ed59b1559f6006c2c544a030c13
Iskamele <iskamele@gmail.com>
2023-02-17 13:02:17 +0200

    Added new get method

--- src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/operationImpl/PurchaseOperation.java
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/operationImpl/PurchaseOperation.java
@@ -2,16 +2,16 @@ package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.operationImp
 
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.database.Storage;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.model.FruitTransaction;
-import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.operationStrategy.OperationHandler;
 
 public class PurchaseOperation implements OperationHandler {
 
     @Override
     public void handleOperation(FruitTransaction transaction) {
-        int oldQuantity = Storage.fruitStorage.get(transaction.getFruit());
+        int oldQuantity = Storage.getFruitStorage().get(transaction.getFruit());
         if (oldQuantity < transaction.getQuantity()) {
             throw new RuntimeException("Not enough fruit. The transaction could not be completed.");
         }
-        Storage.fruitStorage.put(transaction.getFruit(), oldQuantity - transaction.getQuantity());
+        Storage.getFruitStorage().put(transaction.getFruit(),
+                oldQuantity - transaction.getQuantity());
     }
 }
