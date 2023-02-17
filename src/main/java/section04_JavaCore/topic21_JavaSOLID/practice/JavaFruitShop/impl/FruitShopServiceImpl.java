bb60a4519b402a05d2933c6abf96c35eb503e5d4
Iskamele <iskamele@gmail.com>
2023-02-17 13:01:15 +0200

    Changed stream > for and refactored code

--- src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/impl/FruitShopServiceImpl.java
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/impl/FruitShopServiceImpl.java
@@ -1,9 +1,9 @@
 package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.impl;
 
-import java.util.List;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.model.FruitTransaction;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.operationStrategy.OperationStrategy;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.service.FruitShopService;
+import java.util.List;
 
 public class FruitShopServiceImpl implements FruitShopService {
     private final OperationStrategy operationStrategy;
@@ -14,7 +14,8 @@ public class FruitShopServiceImpl implements FruitShopService {
 
     @Override
     public void transactionProcess(List<FruitTransaction> transactionsList) {
-        transactionsList.forEach(fruitTransaction -> operationStrategy.get(fruitTransaction.getOperation())
-                .handleOperation(fruitTransaction));
+        for (FruitTransaction fruitTransaction : transactionsList) {
+            operationStrategy.handleOperation(fruitTransaction);
+        }
     }
 }
