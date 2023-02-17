ea132e5dd4dc72fd18550c2b973e74ab170a5ee0
Iskamele <iskamele@gmail.com>
2023-02-17 13:03:02 +0200

    Refactored code

--- src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/operationStrategy/OperationStrategy.java
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/operationStrategy/OperationStrategy.java
@@ -1,16 +1,22 @@
 package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.operationStrategy;
 
-import java.util.Map;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.model.FruitTransaction;
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.operationImpl.OperationHandler;
+import java.util.Map;
 
 public class OperationStrategy {
-    Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
+    private final Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap;
 
-    public OperationStrategy(Map<FruitTransaction.Operation, OperationHandler> operationHandlerMap) {
+    public OperationStrategy(Map<FruitTransaction.Operation,
+            OperationHandler> operationHandlerMap) {
         this.operationHandlerMap = operationHandlerMap;
     }
 
-    public OperationHandler get(FruitTransaction.Operation operation) {
-        return operationHandlerMap.get(operation);
+    public OperationHandler get(FruitTransaction fruitTransaction) {
+        return operationHandlerMap.get(fruitTransaction.getOperation());
+    }
+
+    public void handleOperation(FruitTransaction fruitTransaction) {
+        get(fruitTransaction).handleOperation(fruitTransaction);
     }
 }
