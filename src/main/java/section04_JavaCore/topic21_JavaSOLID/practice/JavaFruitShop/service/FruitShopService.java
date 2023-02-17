325002cd25cc4936f383849f1cee49db6cda0945
Iskamele <iskamele@gmail.com>
2023-02-17 13:06:26 +0200

    Codestyle fix

--- src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/service/FruitShopService.java
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/service/FruitShopService.java
@@ -1,7 +1,7 @@
 package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.service;
 
-import java.util.List;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.model.FruitTransaction;
+import java.util.List;
 
 public interface FruitShopService {
     void transactionProcess(List<FruitTransaction> fruitTransactionList);
