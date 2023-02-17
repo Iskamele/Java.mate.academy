8818e48f5ed82bdcabd1543f00dc8f62b6587b08
Iskamele <iskamele@gmail.com>
2023-02-17 12:05:29 +0200

    Changed names of variables and class

---
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/impl/FileReaderServiceImpl.java
@@ -0,0 +1,20 @@
+package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.impl;
+
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.service.FileReaderService;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileReaderServiceImpl implements FileReaderService {
+
+    @Override
+    public List<String> readCsvFile(String fromFilePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFilePath))) {
+            return reader.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file " + fromFilePath, e);
+        }
+    }
+}
