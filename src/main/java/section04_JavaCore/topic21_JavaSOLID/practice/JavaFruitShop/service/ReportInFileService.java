79192b2186d6b74de536724f573223865e8a479c
Iskamele <iskamele@gmail.com>
2023-02-17 13:06:13 +0200

    Changed interface name

--- src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/service/ReportInFileService.java
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShop/service/ReportInFileService.java
@@ -1,5 +1,5 @@
 package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShop.service;
 
-public interface ReportInCsvService {
+public interface ReportInFileService {
     String getReport();
 }
