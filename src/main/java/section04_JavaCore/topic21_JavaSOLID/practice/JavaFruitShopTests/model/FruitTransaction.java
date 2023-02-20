ae804867b1f910258e8cdf247f621a0118b86649
Iskamele <iskamele@gmail.com>
2023-02-20 15:20:09 +0200

    Overridden toString, hashCode and equals

--- src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShopTests/model/FruitTransaction.java
+++ src/main/java/section04_JavaCore/topic21_JavaSOLID/practice/JavaFruitShopTests/model/FruitTransaction.java
@@ -1,5 +1,7 @@
 package section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.model;
 
+import java.util.Objects;
+
 public class FruitTransaction {
     private final Operation operation;
     private final String fruit;
@@ -21,6 +23,33 @@ public class FruitTransaction {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FruitTransaction that = (FruitTransaction) o;
+        return quantity == that.quantity
+                && operation == that.operation
+                && Objects.equals(fruit, that.fruit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(operation, fruit, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "FruitTransaction: "
+                + "operation = " + operation
+                + ", fruit = " + fruit
+                + ", quantity = " + quantity;
     }
 
     public enum Operation {
