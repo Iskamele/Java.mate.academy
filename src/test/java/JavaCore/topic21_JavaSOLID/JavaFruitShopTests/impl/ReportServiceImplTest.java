81f605313ddfd218be86da753a78ca59bfd42ead
Iskamele <iskamele@gmail.com>
2023-02-23 22:24:48 +0200

    Added UNit tests for ReportServiceImpl class.

---
+++ src/test/java/JavaCore/topic21_JavaSOLID/JavaFruitShopTests/impl/ReportServiceImplTest.java
@@ -0,0 +1,70 @@
+package JavaCore.topic21_JavaSOLID.JavaFruitShopTests.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.database.Storage;
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.impl.ReportServiceImpl;
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.service.ReportService;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ReportServiceImplTest {
+    private static Map<String, Integer> fruitStorage;
+    private static ReportService reportService;
+
+    @BeforeClass
+    public static void beforeClass() {
+        mockStatic(Storage.class);
+        fruitStorage = new HashMap<>();
+    }
+
+    @Before
+    public void setUp() {
+        reportService = new ReportServiceImpl();
+    }
+
+    @After
+    public void clearStorage() {
+        fruitStorage.clear();
+    }
+
+    @Test
+    public void getReport_getReportSomeData_Ok() {
+        //arrange
+        fruitStorage.put("banana", 50);
+        fruitStorage.put("apple", 200);
+        String expected = "fruit,quantity" + System.lineSeparator()
+                + "banana,50" + System.lineSeparator()
+                + "apple,200" + System.lineSeparator();
+
+        when(Storage.getFruitStorage()).thenReturn(fruitStorage);
+
+        //act
+        String actual = reportService.getReport();
+
+        //assert
+        assertEquals("Incorrect data after writing report: ",
+                expected, actual);
+    }
+
+    @Test
+    public void getReport_getReportEmptyStorage_Ok() {
+        //arrange
+        String expected = "fruit,quantity" + System.lineSeparator();
+        when(Storage.getFruitStorage()).thenReturn(fruitStorage);
+
+        //act
+        String actual = reportService.getReport();
+
+        //assert
+        assertEquals("Incorrect data after writing report: ",
+                expected, actual);
+    }
+}
+
