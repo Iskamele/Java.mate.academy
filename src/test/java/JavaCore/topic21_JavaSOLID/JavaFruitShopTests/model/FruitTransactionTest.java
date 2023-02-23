7f3b0a20c5cd42d72c44434337472961164131d6
Iskamele <iskamele@gmail.com>
2023-02-23 22:24:57 +0200

    Added UNit tests for FruitTransaction class.

---
+++ src/test/java/JavaCore/topic21_JavaSOLID/JavaFruitShopTests/model/FruitTransactionTest.java
@@ -0,0 +1,43 @@
+package JavaCore.topic21_JavaSOLID.JavaFruitShopTests.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.model.FruitTransaction;
+import org.junit.Test;
+
+public class FruitTransactionTest {
+    private static final FruitTransaction.Operation OPERATION =
+            FruitTransaction.Operation.SUPPLY;
+    private static final String INVALID_OPERATION = "i";
+    private static final String FRUIT = "banana";
+    private static final int QUANTITY = 50;
+
+    private static FruitTransaction fruitTransaction;
+
+    @Test
+    public void createFruitTransaction_Ok() {
+        //arrange
+        fruitTransaction = new FruitTransaction(OPERATION, FRUIT, QUANTITY);
+
+        //assert
+        assertEquals("Incorrect operation: ",
+                OPERATION, fruitTransaction.getOperation());
+        assertEquals("Incorrect fruit: ",
+                FRUIT, fruitTransaction.getFruit());
+        assertEquals("Incorrect quantity: ",
+                QUANTITY, fruitTransaction.getQuantity());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void createWithInvalidOperation_NotOk() {
+        //arrange
+        fruitTransaction = new FruitTransaction(
+                FruitTransaction.Operation.getByCode(INVALID_OPERATION),
+                FRUIT,
+                QUANTITY);
+
+        //assert
+        fail("Non-existent operation is not allowed.");
+    }
+}
