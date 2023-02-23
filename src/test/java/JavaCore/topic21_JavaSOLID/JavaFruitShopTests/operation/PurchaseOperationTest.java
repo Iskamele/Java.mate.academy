36de6919d03190ae404a365375a838124282cc44
Iskamele <iskamele@gmail.com>
2023-02-23 22:25:11 +0200

    Added UNit tests for PurchaseOperation class.

---
+++ src/test/java/JavaCore/topic21_JavaSOLID/JavaFruitShopTests/operation/PurchaseOperationTest.java
@@ -0,0 +1,76 @@
+package JavaCore.topic21_JavaSOLID.JavaFruitShopTests.operation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.database.Storage;
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.model.FruitTransaction;
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.operation.OperationHandler;
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.operation.PurchaseOperation;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PurchaseOperationTest {
+    private static OperationHandler operationHandler;
+    private static final String FRUIT = "banana";
+    private static final Integer FIRST_QUANTITY = 100;
+    private static final Integer SECOND_QUANTITY = 30;
+
+    @Before
+    public void setUp() {
+        operationHandler = new PurchaseOperation();
+    }
+
+    @After
+    public void tearDown() {
+        Storage.getFruitStorage().clear();
+    }
+
+    @Test
+    public void handleOperation_getPurchase_Ok() {
+        //arrange
+        Storage.getFruitStorage().put(FRUIT, FIRST_QUANTITY);
+        FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
+                FRUIT, SECOND_QUANTITY);
+        Integer expected = FIRST_QUANTITY - SECOND_QUANTITY;
+
+        //act
+        operationHandler.handleOperation(transaction);
+        Integer actual = Storage.getFruitStorage().get(FRUIT);
+
+        //assert
+        assertEquals("PurchaseOperation should update a quantity in DB.",
+                expected, actual);
+    }
+
+    @Test
+    public void handleOperation_addNewFruitInStorage_Ok() {
+        //arrange
+        FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
+                FRUIT, SECOND_QUANTITY);
+        Integer expected = SECOND_QUANTITY;
+
+        //act
+        operationHandler.handleOperation(transaction);
+        Integer actual = Storage.getFruitStorage().get(FRUIT);
+
+        //assert
+        assertEquals("PurchaseOperation should add new transaction in DB.",
+                expected, actual);
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void handleOperation_purchaseMoreThanKeptInStorage_NotOk() {
+        //arrange
+        Storage.getFruitStorage().put(FRUIT, SECOND_QUANTITY);
+        FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
+                FRUIT, FIRST_QUANTITY);
+
+        //act
+        operationHandler.handleOperation(transaction);
+
+        //assert
+        fail("Not enough fruit. The transaction could not be completed.");
+    }
+}
