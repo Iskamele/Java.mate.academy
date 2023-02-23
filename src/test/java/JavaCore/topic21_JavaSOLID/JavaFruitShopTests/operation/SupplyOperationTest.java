d500ad81319e9dd7d618cf465bd847200bd761a1
Iskamele <iskamele@gmail.com>
2023-02-23 22:25:25 +0200

    Added UNit tests for SupplyOperation class.

---
+++ src/test/java/JavaCore/topic21_JavaSOLID/JavaFruitShopTests/operation/SupplyOperationTest.java
@@ -0,0 +1,61 @@
+package JavaCore.topic21_JavaSOLID.JavaFruitShopTests.operation;
+
+import static org.junit.Assert.assertEquals;
+
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.database.Storage;
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.model.FruitTransaction;
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.operation.OperationHandler;
+import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.operation.SupplyOperation;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SupplyOperationTest {
+    private static OperationHandler operationHandler;
+    private static final String FRUIT = "banana";
+    private static final Integer FIRST_QUANTITY = 100;
+    private static final Integer SECOND_QUANTITY = 30;
+
+    @Before
+    public void setUp() {
+        operationHandler = new SupplyOperation();
+    }
+
+    @After
+    public void tearDown() {
+        Storage.getFruitStorage().clear();
+    }
+
+    @Test
+    public void handleOperation_getSupply_Ok() {
+        //arrange
+        Storage.getFruitStorage().put(FRUIT, FIRST_QUANTITY);
+        FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
+                FRUIT, SECOND_QUANTITY);
+        Integer expected = FIRST_QUANTITY + SECOND_QUANTITY;
+
+        //act
+        operationHandler.handleOperation(transaction);
+        Integer actual = Storage.getFruitStorage().get(FRUIT);
+
+        //assert
+        assertEquals("SupplyOperation should update a quantity in DB.",
+                expected, actual);
+    }
+
+    @Test
+    public void handleOperation_addNewFruitInStorage_Ok() {
+        //arrange
+        FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
+                FRUIT, SECOND_QUANTITY);
+        Integer expected = SECOND_QUANTITY;
+
+        //act
+        operationHandler.handleOperation(transaction);
+        Integer actual = Storage.getFruitStorage().get(FRUIT);
+
+        //assert
+        assertEquals("SupplyOperation should add new transaction in DB.",
+                expected, actual);
+    }
+}
