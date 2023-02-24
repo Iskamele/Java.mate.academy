9fb537a9d5b0af2c9b733a47c7117e8f022f2be9
Iskamele <iskamele@gmail.com>
2023-02-24 20:06:46 +0200

    Added mock for static Storage.

--- src/test/java/JavaCore/topic21_JavaSOLID/JavaFruitShopTests/operation/SupplyOperationTest.java
+++ src/test/java/JavaCore/topic21_JavaSOLID/JavaFruitShopTests/operation/SupplyOperationTest.java
@@ -1,20 +1,35 @@
 package JavaCore.topic21_JavaSOLID.JavaFruitShopTests.operation;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.database.Storage;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.model.FruitTransaction;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.operation.OperationHandler;
 import section04_JavaCore.topic21_JavaSOLID.practice.JavaFruitShopTests.operation.SupplyOperation;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.MockedStatic;
 
 public class SupplyOperationTest {
     private static OperationHandler operationHandler;
     private static final String FRUIT = "banana";
     private static final Integer FIRST_QUANTITY = 100;
     private static final Integer SECOND_QUANTITY = 30;
+    private static Map<String, Integer> fruitStorage;
+    private static MockedStatic<Storage> mockStorage;
+
+    @BeforeClass
+    public static void beforeClass() {
+        mockStorage = mockStatic(Storage.class);
+        fruitStorage = new HashMap<>();
+    }
 
     @Before
     public void setUp() {
@@ -22,21 +37,27 @@ public class SupplyOperationTest {
     }
 
     @After
-    public void tearDown() {
-        Storage.getFruitStorage().clear();
+    public void storageClear() {
+        fruitStorage.clear();
+    }
+
+    @AfterClass
+    public static void closeMock() {
+        mockStorage.close();
     }
 
     @Test
     public void handleOperation_getSupply_Ok() {
         //arrange
-        Storage.getFruitStorage().put(FRUIT, FIRST_QUANTITY);
+        fruitStorage.put(FRUIT, FIRST_QUANTITY);
         FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
                 FRUIT, SECOND_QUANTITY);
         Integer expected = FIRST_QUANTITY + SECOND_QUANTITY;
+        when(Storage.getFruitStorage()).thenReturn(fruitStorage);
 
         //act
         operationHandler.handleOperation(transaction);
-        Integer actual = Storage.getFruitStorage().get(FRUIT);
+        Integer actual = fruitStorage.get(FRUIT);
 
         //assert
         assertEquals("SupplyOperation should update a quantity in DB.",
@@ -49,10 +70,11 @@ public class SupplyOperationTest {
         FruitTransaction transaction = new FruitTransaction(FruitTransaction.Operation.BALANCE,
                 FRUIT, SECOND_QUANTITY);
         Integer expected = SECOND_QUANTITY;
+        when(Storage.getFruitStorage()).thenReturn(fruitStorage);
 
         //act
         operationHandler.handleOperation(transaction);
-        Integer actual = Storage.getFruitStorage().get(FRUIT);
+        Integer actual = fruitStorage.get(FRUIT);
 
         //assert
         assertEquals("SupplyOperation should add new transaction in DB.",
