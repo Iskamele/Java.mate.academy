68b9f2178c47454a8f9cf40243ca7c0a8cf7c6d0
Iskamele <iskamele@gmail.com>
2023-02-25 20:00:33 +0200

    Difference between DIP, DI, and IoC, theory

---
+++ src/main/java/section04_JavaCore/topic22_JavaDependencyInjection/theory/T06_DifferenceBetweenDIPDIAndIoC/Main.java
@@ -0,0 +1,10 @@
+package section04_JavaCore.topic22_JavaDependencyInjection.theory.T06_DifferenceBetweenDIPDIAndIoC;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Dependency Inversion - we should depend on abstractions
+        // Dependency Injection - we should initialize dependencies outside
+        // Inversion of Control - someone else will call new
+    }
+}
