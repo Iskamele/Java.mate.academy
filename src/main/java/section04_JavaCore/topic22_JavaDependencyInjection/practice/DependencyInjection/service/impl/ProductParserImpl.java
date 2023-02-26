a5f081c4fdeb91c333f5b69381f9dbfb73dd88de
Iskamele <iskamele@gmail.com>
2023-02-26 12:10:41 +0200

    Added Product parser implementation from interface.

---
+++ src/main/java/section04_JavaCore/topic22_JavaDependencyInjection/practice/DependencyInjection/service/impl/ProductParserImpl.java
@@ -0,0 +1,27 @@
+package section04_JavaCore.topic22_JavaDependencyInjection.practice.DependencyInjection.service.impl;
+
+import java.math.BigDecimal;
+import section04_JavaCore.topic22_JavaDependencyInjection.practice.DependencyInjection.lib.Component;
+import section04_JavaCore.topic22_JavaDependencyInjection.practice.DependencyInjection.model.Product;
+import section04_JavaCore.topic22_JavaDependencyInjection.practice.DependencyInjection.service.ProductParser;
+
+@Component
+public class ProductParserImpl implements ProductParser {
+    public static final int ID_POSITION = 0;
+    public static final int NAME_POSITION = 1;
+    public static final int CATEGORY_POSITION = 2;
+    public static final int DESCRIPTION_POSITION = 3;
+    public static final int PRICE_POSITION = 4;
+
+    @Override
+    public Product parse(String productInfo) {
+        String[] data = productInfo.split(",");
+        Product product = new Product();
+        product.setId(Long.valueOf(data[ID_POSITION]));
+        product.setName(data[NAME_POSITION]);
+        product.setCategory(data[CATEGORY_POSITION]);
+        product.setDescription(data[DESCRIPTION_POSITION]);
+        product.setPrice(new BigDecimal(data[PRICE_POSITION]));
+        return product;
+    }
+}
