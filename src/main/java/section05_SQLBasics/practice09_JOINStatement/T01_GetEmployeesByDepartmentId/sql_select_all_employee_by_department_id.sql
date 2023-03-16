bab8947354e6bf7772908d7f8e515323f9a51fad
Iskamele <iskamele@gmail.com>
2023-03-16 15:52:23 +0200

    Get employees by department id practice.

---
+++ src/main/java/section05_SQLBasics/practice09_JOINStatement/T01_GetEmployeesByDepartmentId/sql_select_all_employee_by_department_id.sql
@@ -0,0 +1,5 @@
+SELECT first_name, last_name, department.name AS department_name
+FROM employee
+         LEFT JOIN department
+                   ON department.id = employee.department_id
+WHERE department.id IN (2, 7);
