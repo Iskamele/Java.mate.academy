4ff09390f06553b8f864c6a877ab6630594139f1
Iskamele <iskamele@gmail.com>
2023-03-21 10:10:40 +0200

    Added main method for tests.

---
+++ src/main/java/section06_JavaJDBC/topic02_JDBCINtro/practice/JDBC/Main.java
@@ -0,0 +1,61 @@
+package section06_JavaJDBC.topic02_JDBCINtro.practice.JDBC;
+
+import section06_JavaJDBC.topic02_JDBCINtro.practice.JDBC.dao.ManufacturerDao;
+import section06_JavaJDBC.topic02_JDBCINtro.practice.JDBC.dao.ManufacturerDaoImpl;
+import section06_JavaJDBC.topic02_JDBCINtro.practice.JDBC.model.Manufacturer;
+
+public class Main {
+    public static void main(String[] args) {
+        ManufacturerDao manufacturerDao = new ManufacturerDaoImpl();
+        /* Test data
+        Manufacturer firstDummyManufacturer = new Manufacturer();
+        firstDummyManufacturer.setId(1L);
+        firstDummyManufacturer.setName("Porsche");
+        firstDummyManufacturer.setCountry("Germany");
+
+        Manufacturer secondDummyManufacturer = new Manufacturer();
+        secondDummyManufacturer.setId(2L);
+        secondDummyManufacturer.setName("Mitsubishi");
+        secondDummyManufacturer.setCountry("Japan");
+
+        Manufacturer thirdDummyManufacturer = new Manufacturer();
+        thirdDummyManufacturer.setId(3L);
+        thirdDummyManufacturer.setName("Zaz");
+        thirdDummyManufacturer.setCountry("Ukraine");
+
+        manufacturerDao.create(firstDummyManufacturer);
+        manufacturerDao.create(secondDummyManufacturer);
+        manufacturerDao.create(thirdDummyManufacturer);
+         */
+
+        Manufacturer newManufacturer = new Manufacturer();
+        newManufacturer.setName("BMW");
+        newManufacturer.setCountry("Germany");
+        Manufacturer createdManufacturer = manufacturerDao.create(newManufacturer);
+        System.out.println(".create() new: " + createdManufacturer + System.lineSeparator());
+
+        Long createdManufacturerId = createdManufacturer.getId();
+        Manufacturer gotManufacturerFromDb = manufacturerDao.get(createdManufacturerId).get();
+        System.out.println(".get() by id: " + gotManufacturerFromDb + System.lineSeparator());
+
+        System.out.println(".getAll() from DB:");
+        manufacturerDao.getAll().forEach(System.out::println);
+
+        Manufacturer updateManufacturer = new Manufacturer();
+        updateManufacturer.setName("Ford");
+        updateManufacturer.setCountry("USA");
+        updateManufacturer.setId(createdManufacturerId);
+        System.out.println(System.lineSeparator() + ".update(): with " + updateManufacturer);
+        System.out.println("Old manufacturer: " + gotManufacturerFromDb);
+        Manufacturer updatedManufacturer = manufacturerDao.update(updateManufacturer);
+        System.out.println("Updated manufacturer: " + updatedManufacturer + System.lineSeparator());
+
+        boolean delete = manufacturerDao.delete(createdManufacturerId);
+        System.out.println(".delete(): for " + updatedManufacturer
+                + " is " + delete + System.lineSeparator());
+
+        System.out.println(".getAll() after .delete() operation: ");
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println(System.lineSeparator());
+    }
+}
