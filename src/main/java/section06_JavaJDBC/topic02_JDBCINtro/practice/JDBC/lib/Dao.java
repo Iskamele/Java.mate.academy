2c30ca5ba9a3d348427868d035018805478e85d2
Iskamele <iskamele@gmail.com>
2023-03-21 10:09:29 +0200

    Added Dao annotation.

---
+++ src/main/java/section06_JavaJDBC/topic02_JDBCINtro/practice/JDBC/lib/Dao.java
@@ -0,0 +1,8 @@
+package section06_JavaJDBC.topic02_JDBCINtro.practice.JDBC.lib;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Dao {
+}
