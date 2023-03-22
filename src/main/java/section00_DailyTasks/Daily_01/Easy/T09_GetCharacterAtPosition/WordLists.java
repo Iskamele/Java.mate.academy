a4ca2f20e432f24cf9177621da5aa6fb6a9b8c9c
Iskamele <iskamele@gmail.com>
2023-03-22 22:13:40 +0200

    Get character at position daily practice.

---
+++ src/main/java/section00_DailyTasks/Daily_01/Easy/T09_GetCharacterAtPosition/WordLists.java
@@ -0,0 +1,16 @@
+
+package section00_DailyTasks.Daily_01.Easy.T09_GetCharacterAtPosition;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class WordLists {
+    public static List<Character> getCharacterAtPosition(List<String> strings, int position) {
+        return strings.stream()
+                .filter(Objects::nonNull)
+                .map(s -> s.toLowerCase().charAt(position))
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}
