0307f6c681ec5ac482586be97b75180935066bcb
Iskamele <iskamele@gmail.com>
2023-04-01 16:46:38 +0300

    Codestyle fix.

--- src/main/java/section05_SQLBasics/practice09_JOINStatement/T07_GetNumberOfEmployeesForEachDepartment/sql_select_number_of_employees_for_each_department.sql
+++ src/main/java/section05_SQLBasics/practice09_JOINStatement/T07_GetNumberOfEmployeesForEachDepartment/sql_select_number_of_employees_for_each_department.sql
@@ -1,5 +1,5 @@
 SELECT department.name AS department_name, COUNT(employee.*) AS number_of_employees
 FROM employee
-         INNER JOIN department
-                    ON employee.department_id = department.id
+INNER JOIN department
+ON employee.department_id = department.id
 GROUP BY department.name;
