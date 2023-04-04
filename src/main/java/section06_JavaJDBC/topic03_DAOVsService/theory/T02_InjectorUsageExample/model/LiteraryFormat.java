2d8ff2646621f4b2feb5a40640dc1f605cb8808e
Iskamele <iskamele@gmail.com>
2023-04-04 11:55:26 +0300

    Create LiteraryFormat entity class

---
+++ src/main/java/section06_JavaJDBC/topic03_DAOVsService/theory/T02_InjectorUsageExample/model/LiteraryFormat.java
@@ -0,0 +1,30 @@
+package section06_JavaJDBC.topic03_DAOVsService.theory.T02_InjectorUsageExample.model;
+
+public class LiteraryFormat {
+    private Long id;
+    public String format;
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "LiteraryFormat{" +
+                "id=" + id +
+                ", format='" + format + '\'' +
+                '}';
+    }
+}
