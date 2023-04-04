963f23bb8c687d5fa9669dc8259d64be2cae149e
Iskamele <iskamele@gmail.com>
2023-04-04 11:55:19 +0300

    Create Service interface

---
+++ src/main/java/section06_JavaJDBC/topic03_DAOVsService/theory/T02_InjectorUsageExample/lib/Service.java
@@ -0,0 +1,8 @@
+package section06_JavaJDBC.topic03_DAOVsService.theory.T02_InjectorUsageExample.lib;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Service {
+}
