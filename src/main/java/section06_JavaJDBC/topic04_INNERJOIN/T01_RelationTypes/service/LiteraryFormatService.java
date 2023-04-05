86eb97b832665fec0a2675acf42ac6f428b2a94a
Iskamele <iskamele@gmail.com>
2023-04-05 11:09:56 +0300

    Add LiteraryFormatService interface for working with literary format data

---
+++ src/main/java/section06_JavaJDBC/topic04_INNERJOIN/T01_RelationTypes/service/LiteraryFormatService.java
@@ -0,0 +1,17 @@
+package section06_JavaJDBC.topic04_INNERJOIN.T01_RelationTypes.service;
+
+import java.util.List;
+import java.util.Optional;
+import section06_JavaJDBC.topic04_INNERJOIN.T01_RelationTypes.model.LiteraryFormat;
+
+public interface LiteraryFormatService {
+    List<LiteraryFormat> getAll();
+
+    LiteraryFormat create(LiteraryFormat format);
+
+    Optional<LiteraryFormat> get(Long id);
+
+    LiteraryFormat update(LiteraryFormat format);
+
+    boolean delete(Long id);
+}
