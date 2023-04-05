dc4fae12f790b37cb654f0647cf2ea945bf8a44e
Iskamele <iskamele@gmail.com>
2023-04-05 11:09:45 +0300

    Add LiteraryFormat class for representing literary format data

---
+++ src/main/java/section06_JavaJDBC/topic04_INNERJOIN/T01_RelationTypes/model/LiteraryFormat.java
@@ -0,0 +1,30 @@
+package section06_JavaJDBC.topic04_INNERJOIN.T01_RelationTypes.model;
+
+public class LiteraryFormat {
+    private Long id;
+    public String format;
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "LiteraryFormat{" +
+                "id=" + id +
+                ", format='" + format + '\'' +
+                '}';
+    }
+}
