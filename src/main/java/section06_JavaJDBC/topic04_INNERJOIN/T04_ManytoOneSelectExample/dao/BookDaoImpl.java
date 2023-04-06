862bcd38a9be6a50756fde8b212228821637df76
Iskamele <iskamele@gmail.com>
2023-04-06 10:01:58 +0300

    Add BookDaoImpl implementation

---
+++ src/main/java/section06_JavaJDBC/topic04_INNERJOIN/T04_ManytoOneSelectExample/dao/BookDaoImpl.java
@@ -0,0 +1,85 @@
+package section06_JavaJDBC.topic04_INNERJOIN.T04_ManytoOneSelectExample.dao;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+import section06_JavaJDBC.topic04_INNERJOIN.T04_ManytoOneSelectExample.model.Book;
+import section06_JavaJDBC.topic04_INNERJOIN.T04_ManytoOneSelectExample.model.LiteraryFormat;
+import section06_JavaJDBC.topic04_INNERJOIN.T04_ManytoOneSelectExample.util.ConnectionUtil;
+
+public class BookDaoImpl implements BookDao {
+    @Override
+    public Book create(Book book) {
+        String query = "INSERT INTO books (title, price, literary_format_id)" +
+                " VALUES (?, ?, ?);";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)
+        ) {
+            preparedStatement.setString(1, book.getTitle());
+            preparedStatement.setBigDecimal(2, book.getPrice());
+            preparedStatement.setLong(3, book.getFormat().getId());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                Long id = resultSet.getObject(1, Long.class);
+                book.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't insert format to DB", e);
+        }
+        return book;
+    }
+
+    @Override
+    public Optional<Book> get(Long id) {
+        String query = "SELECT b.id AS book_id, title, price, lf.format, lf.id AS literary_format_id " +
+                "FROM books b " +
+                "JOIN literary_formats lf " +
+                "ON b.literary_format_id = lf.id " +
+                "WHERE b.id = ?;";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)
+        ) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+//                Book book = getBook(resultSet);
+                String title = resultSet.getNString("title");
+                BigDecimal price = resultSet.getBigDecimal("price");
+                LiteraryFormat literaryFormat = new LiteraryFormat();
+                literaryFormat.setId(resultSet.getObject("literary_format_id", Long.class));
+                literaryFormat.setFormat(resultSet.getString("format"));
+                Book book = new Book();
+                book.setId(id);
+                book.setTitle(title);
+                book.setPrice(price);
+                book.setFormat(literaryFormat);
+                return Optional.of(book);
+            } else {
+                return Optional.empty();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't insert format to DB", e);
+        }
+    }
+
+//    private Book getBook(ResultSet resultSet) throws SQLException {
+//        Long id = resultSet.getObject(1, Long.class);
+//        String title = resultSet.getNString("title");
+//        BigDecimal price = resultSet.getBigDecimal("price");
+//        LiteraryFormat literaryFormat = new LiteraryFormat();
+//        literaryFormat.setId(resultSet.getObject("literary_format_id", Long.class));
+//        literaryFormat.setFormat(resultSet.getString("format"));
+//        Book book = new Book();
+//        book.setId(id);
+//        book.setTitle(title);
+//        book.setPrice(price);
+//        book.setFormat(literaryFormat);
+//        return book;
+//    }
+}
