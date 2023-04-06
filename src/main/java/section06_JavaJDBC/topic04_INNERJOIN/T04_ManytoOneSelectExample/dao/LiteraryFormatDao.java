24c6ebb1ced1e5313a36ba125dfafeb78d86bbbd
Iskamele <iskamele@gmail.com>
2023-04-06 10:02:13 +0300

    Add LiteraryFormatDao interface

---
+++ src/main/java/section06_JavaJDBC/topic04_INNERJOIN/T04_ManytoOneSelectExample/dao/LiteraryFormatDao.java
@@ -0,0 +1,17 @@
+package section06_JavaJDBC.topic04_INNERJOIN.T04_ManytoOneSelectExample.dao;
+
+import java.util.List;
+import java.util.Optional;
+import section06_JavaJDBC.topic04_INNERJOIN.T04_ManytoOneSelectExample.model.LiteraryFormat;
+
+public interface LiteraryFormatDao {
+    List<LiteraryFormat> getAll();
+
+    LiteraryFormat create(LiteraryFormat format);
+
+    Optional<LiteraryFormat> get(Long id);
+
+    LiteraryFormat update(LiteraryFormat format);
+
+    boolean delete(Long id);
+}
