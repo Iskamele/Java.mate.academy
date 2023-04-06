0ecce4b10713961276cedc1a9013cd6d2a9f89fa
Iskamele <iskamele@gmail.com>
2023-04-06 10:01:48 +0300

    Add BookDao interface

---
+++ src/main/java/section06_JavaJDBC/topic04_INNERJOIN/T05_ManytoOneDeleteExample/dao/BookDao.java
@@ -0,0 +1,12 @@
+package section06_JavaJDBC.topic04_INNERJOIN.T05_ManytoOneDeleteExample.dao;
+
+import java.util.Optional;
+import section06_JavaJDBC.topic04_INNERJOIN.T05_ManytoOneDeleteExample.model.Book;
+
+public interface BookDao {
+    Book create(Book book);
+
+    Optional<Book> get(Long id);
+
+    boolean delete(String title);
+}
