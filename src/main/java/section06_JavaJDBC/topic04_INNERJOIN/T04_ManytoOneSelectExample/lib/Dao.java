e352d3a6532d048354b242be9b08c0c5bb468a22
Iskamele <iskamele@gmail.com>
2023-04-06 10:02:55 +0300

    Add Dao annotation

---
+++ src/main/java/section06_JavaJDBC/topic04_INNERJOIN/T04_ManytoOneSelectExample/lib/Dao.java
@@ -0,0 +1,8 @@
+package section06_JavaJDBC.topic04_INNERJOIN.T04_ManytoOneSelectExample.lib;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Dao {
+}
