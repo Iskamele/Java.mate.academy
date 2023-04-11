feb05a84788cfab1d95fd8131380d1dfea73322a
Iskamele <iskamele@gmail.com>
2023-04-11 16:20:46 +0300

    Servlet lifecycle theory.

---
+++ src/main/java/section07_JavaWeb/topic02_ServletLifeCycle/practice/ServletLifecycle/controller/IndexController.java
@@ -0,0 +1,31 @@
+package section07_JavaWeb.topic02_ServletLifeCycle.practice.ServletLifecycle.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import section07_JavaWeb.topic02_ServletLifeCycle.practice.ServletLifecycle.dao.MyCoolResource;
+
+//@WebServlet(urlPatterns = {"/index"})
+public class IndexController extends HttpServlet {
+    private static final String INDEX_JSP_PATH = "/WEB-INF/views/index.jsp";
+    private MyCoolResource myResource;
+
+    @Override
+    public void init() throws ServletException {
+        myResource = MyCoolResource.openResource();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        myResource.write(java.time.LocalDateTime.now().toString());
+        req.getRequestDispatcher(INDEX_JSP_PATH).forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        myResource.close();
+    }
+}
