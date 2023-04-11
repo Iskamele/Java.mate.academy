a3b062008bc070903367b60ce084146769bb8be5
Iskamele <iskamele@gmail.com>
2023-04-11 16:18:25 +0300

    War vs Jar theory.

---
+++ src/main/java/section07_JavaWeb/topic01_WebIntro/theory/T06_WarVsJar/Main.java
@@ -0,0 +1,10 @@
+package section07_JavaWeb.topic01_WebIntro.theory.T06_WarVsJar;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("JAR was launched...");
+        System.out.println("Input arguments: " + Arrays.toString(args));
+    }
+}
