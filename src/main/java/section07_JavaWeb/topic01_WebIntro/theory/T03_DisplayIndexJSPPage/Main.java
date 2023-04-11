f4686c69d65652a445182d3dbcec21eeba726069
Iskamele <iskamele@gmail.com>
2023-04-11 16:17:21 +0300

    Display index jsp page theory.

---
+++ src/main/java/section07_JavaWeb/topic01_WebIntro/theory/T03_DisplayIndexJSPPage/Main.java
@@ -0,0 +1,13 @@
+package section07_JavaWeb.topic01_WebIntro.theory.T03_DisplayIndexJSPPage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Display a page in browser
+        //    - add required dependency
+        //    - create Servlet class
+        //    - configure web.xml
+        //    - create index.jsp page
+        //    - run web app
+    }
+}
