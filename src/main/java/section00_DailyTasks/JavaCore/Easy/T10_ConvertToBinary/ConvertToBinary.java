5a5baa53ca5703ffd79609167065a7384ffb32ba
Iskamele <iskamele@gmail.com>
2023-04-12 13:28:30 +0300

    Convert to binary daily task.

---
+++ src/main/java/section00_DailyTasks/JavaCore/Easy/T10_ConvertToBinary/ConvertToBinary.java
@@ -0,0 +1,8 @@
+
+package section00_DailyTasks.JavaCore.Easy.T10_ConvertToBinary;
+
+public class ConvertToBinary {
+    public static int toBinary(int num) {
+        return Integer.parseInt(Integer.toBinaryString(num));
+    }
+}
