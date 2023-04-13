6c0c038e10be334aec115dfe0d921254bae2482d
Iskamele <iskamele@gmail.com>
2023-04-13 13:53:01 +0300

    Implemented DriverService interface.

---
+++ src/main/java/section07_JavaWeb/topic03_WebPractice/practice/WebPractice/service/DriverService.java
@@ -0,0 +1,6 @@
+package section07_JavaWeb.topic03_WebPractice.practice.WebPractice.service;
+
+import section07_JavaWeb.topic03_WebPractice.practice.WebPractice.model.Driver;
+
+public interface DriverService extends GenericService<Driver> {
+}
