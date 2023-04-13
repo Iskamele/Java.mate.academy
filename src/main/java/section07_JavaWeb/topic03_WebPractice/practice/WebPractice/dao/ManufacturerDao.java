1fc2f5dbc4a03162bf3025033758365398131145
Iskamele <iskamele@gmail.com>
2023-04-13 13:52:04 +0300

    Implemented ManufacturerDao interface.

---
+++ src/main/java/section07_JavaWeb/topic03_WebPractice/practice/WebPractice/dao/ManufacturerDao.java
@@ -0,0 +1,6 @@
+package section07_JavaWeb.topic03_WebPractice.practice.WebPractice.dao;
+
+import section07_JavaWeb.topic03_WebPractice.practice.WebPractice.model.Manufacturer;
+
+public interface ManufacturerDao extends GenericDao<Manufacturer> {
+}
