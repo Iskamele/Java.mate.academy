1221b053eb9fca2ad64571e3a14cd1bf43254f6f
Iskamele <iskamele@gmail.com>
2023-04-13 13:53:17 +0300

    Added ConnectionUtil.

---
+++ src/main/java/section07_JavaWeb/topic03_WebPractice/practice/WebPractice/util/ConnectionUtil.java
@@ -0,0 +1,32 @@
+package section07_JavaWeb.topic03_WebPractice.practice.WebPractice.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/taxi";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "8CMT3f~8F4H_2v+f4py^d7+3p%MP)!fG";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find SQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.setProperty("user", USERNAME);
+        dbProperties.setProperty("password", PASSWORD);
+        try {
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB ", e);
+        }
+    }
+}
