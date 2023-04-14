229f57e54433273eed79eec6082d91ad854a5257
Iskamele <iskamele@gmail.com>
2023-04-14 14:15:25 +0300

    Added Get my current car controller.

---
+++ src/main/java/section07_JavaWeb/topic04_SecurityInWeb/practice/WebSecurity/controller/car/GetMyCurrentCarsController.java
@@ -0,0 +1,26 @@
+package section07_JavaWeb.topic04_SecurityInWeb.practice.WebSecurity.controller.car;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import section07_JavaWeb.topic04_SecurityInWeb.practice.WebSecurity.lib.Injector;
+import section07_JavaWeb.topic04_SecurityInWeb.practice.WebSecurity.model.Car;
+import section07_JavaWeb.topic04_SecurityInWeb.practice.WebSecurity.service.CarService;
+
+//@WebServlet(urlPatterns = "/cars/my")
+public class GetMyCurrentCarsController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Long driverId = (Long) req.getSession().getAttribute("driver_id");
+        List<Car> cars = carService.getAllByDriver(driverId);
+        req.setAttribute("cars", cars);
+        req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
+    }
+}
