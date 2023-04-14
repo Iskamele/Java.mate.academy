75b196595ad537d9a0e447a4afb16562c2b321f3
Iskamele <iskamele@gmail.com>
2023-04-14 14:17:03 +0300

    Added Dao, Inject and Service annotations.

---
+++ src/main/java/section07_JavaWeb/topic04_SecurityInWeb/practice/WebSecurity/lib/Service.java
@@ -0,0 +1,8 @@
+package section07_JavaWeb.topic04_SecurityInWeb.practice.WebSecurity.lib;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Service {
+}
