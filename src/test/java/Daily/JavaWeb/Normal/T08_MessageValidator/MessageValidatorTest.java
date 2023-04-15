632c5c72ae41460dc6c6a3a3988e687bb3b31d69
Iskamele <iskamele@gmail.com>
2023-04-15 10:01:57 +0300

    Added test for Message validator.

---
+++ src/test/java/Daily/JavaWeb/Normal/T08_MessageValidator/MessageValidatorTest.java
@@ -0,0 +1,37 @@
+package Daily.JavaWeb.Normal.T08_MessageValidator;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import section00_DailyTasks.JavaWeb.Normal.T08_MessageValidator.MessageValidator;
+
+public class MessageValidatorTest {
+    private static final Map<String, Boolean> data = new HashMap<>();
+
+    @BeforeClass
+    public static void beforeClass() {
+        data.put("", true);
+        data.put("0", true);
+        data.put("1a", true);
+        data.put("2aa3bb", false);
+        data.put("2aa3bbb", true);
+        data.put("3hey5hello2hi5", false);
+        data.put("code4hello5", false);
+        data.put("3hey5hello2hi", true);
+        data.put("11mateacademy", true);
+        data.put("1a2bb3ccc4dddd5eeeeee", false);
+    }
+
+    @Test
+    public void isValidMessage_checkString() {
+        for (Map.Entry<String, Boolean> entry : data.entrySet()) {
+            String input = entry.getKey();
+            boolean expected = entry.getValue();
+            boolean actual = MessageValidator.isValidMessage(input);
+            Assert.assertEquals(String.format("Your method should return %b for the input string \"%s\"\n",
+                    expected, input), expected, actual);
+        }
+    }
+}
