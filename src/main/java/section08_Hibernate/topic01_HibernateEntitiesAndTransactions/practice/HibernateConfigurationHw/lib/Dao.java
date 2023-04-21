64176a9132bc3627608f3c9b21a192c98bf335e4
Iskamele <iskamele@gmail.com>
2023-04-21 15:21:16 +0300

    Added dao, inject and service annotations.

---
+++ src/main/java/section08_Hibernate/topic01_HibernateEntitiesAndTransactions/practice/HibernateConfigurationHw/lib/Dao.java
@@ -0,0 +1,8 @@
+package section08_Hibernate.topic01_HibernateEntitiesAndTransactions.practice.HibernateConfigurationHw.lib;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Dao {
+}
