8487c4b742f62623ecab55a56cf7b820acda5a3e
Iskamele <iskamele@gmail.com>
2023-04-23 21:41:47 +0300

    Simple multiplication mentor's implementation, daily task.

---
+++ src/main/java/section00_DailyTasks/JavaCore/Easy/T13_SimpleMultiplication/Mentor/SimpleMultiplication.java
@@ -0,0 +1,7 @@
+package section00_DailyTasks.JavaCore.Easy.T13_SimpleMultiplication.Mentor;
+
+public class SimpleMultiplication {
+    public static int multiplyNumber(int num) {
+        return num % 2 == 0 ? num * 8 : num * 9;
+    }
+}
