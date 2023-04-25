d633fe951a03c93d4f268f25007df4a796d314be
Iskamele <iskamele@gmail.com>
2023-04-25 19:03:30 +0300

    Added abstract dao class.

---
+++ src/main/java/section08_Hibernate/topic02_EntityRelationsAndLifeCycle/practice/HibernateRelations/dao/impl/AbstractDao.java
@@ -0,0 +1,11 @@
+package section08_Hibernate.topic02_EntityRelationsAndLifeCycle.practice.HibernateRelations.dao.impl;
+
+import org.hibernate.SessionFactory;
+
+public abstract class AbstractDao {
+    protected final SessionFactory factory;
+
+    protected AbstractDao(SessionFactory sessionFactory) {
+        this.factory = sessionFactory;
+    }
+}
