b11956d3edc5d2e6dad22a6c8b121987ef484988
Iskamele <iskamele@gmail.com>
2023-04-27 21:39:43 +0300

    Round Numbers mentor's implementation, daily task.

---
+++ src/main/java/section00_DailyTasks/JavaCore/Easy/T04_RoundToHundredth/Mentor/RoundNumbers.java
@@ -0,0 +1,7 @@
+package section00_DailyTasks.JavaCore.Easy.T04_RoundToHundredth.Mentor;
+
+public class RoundNumbers {
+    public static double roundToHundredth(double num) {
+        return (Math.round(num * 100)) / 100.0;
+    }
+}
