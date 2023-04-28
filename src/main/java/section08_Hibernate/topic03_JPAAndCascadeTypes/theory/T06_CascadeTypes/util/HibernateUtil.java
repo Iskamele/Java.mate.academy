2c3b54d0cafdf4764a66b36dc13c51ee61d7599f
Iskamele <iskamele@gmail.com>
2023-04-28 14:31:25 +0300

    Cascade types theory.

---
+++ src/main/java/section08_Hibernate/topic03_JPAAndCascadeTypes/theory/T06_CascadeTypes/util/HibernateUtil.java
@@ -0,0 +1,19 @@
+package section08_Hibernate.topic03_JPAAndCascadeTypes.theory.T06_CascadeTypes.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory instance = initSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory initSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return instance;
+    }
+}
