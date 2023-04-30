2b79a6da4a922eb31d6e714171d1e7f4aa9d571d
Iskamele <iskamele@gmail.com>
2023-04-30 16:44:22 +0300

    Added project models.

---
+++ src/main/java/section08_Hibernate/topic03_JPAAndCascadeTypes/practice/HibernateCascade/model/Comment.java
@@ -0,0 +1,44 @@
+package section08_Hibernate.topic03_JPAAndCascadeTypes.practice.HibernateCascade.model;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    @OneToMany
+    private List<Smile> smiles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Smile> getSmiles() {
+        return smiles;
+    }
+
+    public void setSmiles(List<Smile> smiles) {
+        this.smiles = smiles;
+    }
+}
---
+++ src/main/java/section08_Hibernate/topic03_JPAAndCascadeTypes/practice/HibernateCascade/model/Message.java
@@ -0,0 +1,44 @@
+package section08_Hibernate.topic03_JPAAndCascadeTypes.practice.HibernateCascade.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "message")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private MessageDetails messageDetails;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public MessageDetails getMessageDetails() {
+        return messageDetails;
+    }
+
+    public void setMessageDetails(MessageDetails messageDetails) {
+        this.messageDetails = messageDetails;
+    }
+}
