4166d5f288aabf21663131f077f29c9a9ab6bbb3
Iskamele <iskamele@gmail.com>
2023-05-01 17:51:54 +0300

    Smash words daily task.

---
+++ src/main/java/section00_DailyTasks/JavaCore/Easy/T14_SmashWords/SmashWords.java
@@ -0,0 +1,7 @@
+package section00_DailyTasks.JavaCore.Easy.T14_SmashWords;
+
+public class SmashWords {
+    public static String makeSentence(String[] words) {
+        return String.join(" ", words);
+    }
+}
