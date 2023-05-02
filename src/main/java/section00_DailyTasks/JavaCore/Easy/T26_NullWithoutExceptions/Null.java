ab5c186cb6afc7ef9397b6c7395bcada2a1ec3f6
Iskamele <iskamele@gmail.com>
2023-05-02 19:46:42 +0300

    Null without exceptions daily task.

---
+++ src/main/java/section00_DailyTasks/JavaCore/Easy/T26_NullWithoutExceptions/Null.java
@@ -0,0 +1,18 @@
+package section00_DailyTasks.JavaCore.Easy.T26_NullWithoutExceptions;
+
+public class Null {
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "null";
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
