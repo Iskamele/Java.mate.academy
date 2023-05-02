1f9883be2c7a2a3f13e457addee41b1b9849c078
Iskamele <iskamele@gmail.com>
2023-05-02 19:45:44 +0300

    Volume of cuboid daily task.

---
+++ src/main/java/section00_DailyTasks/JavaCore/Easy/T21_VolumeOfCuboid/Cuboid.java
@@ -0,0 +1,7 @@
+package section00_DailyTasks.JavaCore.Easy.T21_VolumeOfCuboid;
+
+public class Cuboid {
+    public static double getVolume(double length, double width, double height) {
+        return length * width * height;
+    }
+}
