5cce9e676a27cfe62164ef6ebb0bdd652a231f89
Iskamele <iskamele@gmail.com>
2023-05-02 19:46:20 +0300

    Opposite number daily task.

---
+++ src/main/java/section00_DailyTasks/JavaCore/Easy/T24_OppositeNumber/OppositeNumber.java
@@ -0,0 +1,7 @@
+package section00_DailyTasks.JavaCore.Easy.T24_OppositeNumber;
+
+public class OppositeNumber {
+    public static int oppositeNumber(int number) {
+        return -number;
+    }
+}
