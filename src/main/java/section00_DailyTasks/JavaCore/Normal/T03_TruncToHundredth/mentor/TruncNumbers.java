32e13b1def5d82e3d448533e7fa02243e6ce12eb
Iskamele <iskamele@gmail.com>
2023-05-04 20:26:59 +0300

    Trunk to hundredth daily task, mentor's implementation.

---
+++ src/main/java/section00_DailyTasks/JavaCore/Normal/T03_TruncToHundredth/mentor/TruncNumbers.java
@@ -0,0 +1,7 @@
+package section00_DailyTasks.JavaCore.Normal.T03_TruncToHundredth.mentor;
+
+public class TruncNumbers {
+    public static double truncateToHundredth(double num) {
+        return (int) (num * 100) / 100.0;
+    }
+}
