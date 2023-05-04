0bfa7f592d6317a8fc542c5068985314c7291222
Iskamele <iskamele@gmail.com>
2023-05-04 20:27:59 +0300

    Break chocolate problem daily task, mentor's implementation.

---
+++ src/main/java/section00_DailyTasks/JavaCore/Normal/T08_BreakChocolateProblem/mentor/ChocolateBar.java
@@ -0,0 +1,7 @@
+package section00_DailyTasks.JavaCore.Normal.T08_BreakChocolateProblem.mentor;
+
+public class ChocolateBar {
+    public static int breakChocolate(int n, int m) {
+        return Math.max(n * m - 1, 0);
+    }
+}
