4c7e2044595aee86b423224c3b45c4f66b220faa
Iskamele <iskamele@gmail.com>
2023-05-09 11:02:31 +0300

    Added injector and annotations.

---
+++ src/main/java/section08_Hibernate/topic04_WorkingWithLazyInitialization/practice/HibernateCreateMovieSession/lib/Dao.java
@@ -0,0 +1,8 @@
+package section08_Hibernate.topic04_WorkingWithLazyInitialization.practice.HibernateCreateMovieSession.lib;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Dao {
+}
