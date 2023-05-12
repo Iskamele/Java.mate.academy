064a6d30b736cfcbf32cad7fce2dfa7dc9e595b1
Iskamele <iskamele@gmail.com>
2023-05-12 11:39:59 +0300

    Added injector and annotations.

---
+++ src/main/java/section08_Hibernate/topic06_TransactionIsolationLevels/practice/HibernateUserService/lib/Inject.java
@@ -0,0 +1,8 @@
+package section08_Hibernate.topic06_TransactionIsolationLevels.practice.HibernateUserService.lib;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject {
+}
