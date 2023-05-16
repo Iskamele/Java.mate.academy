5ba542bf438d0964b47e8073250693fbac23294c
Iskamele <iskamele@gmail.com>
2023-05-16 21:50:02 +0300

    Get length of the values task.

---
+++ src/main/java/section00_DailyTasks/JavaHibernate/Normal/T03_GetLengthOfTheValues/sql_get_length_of_the_values.sql
@@ -0,0 +1,7 @@
+SELECT
+    CHAR_LENGTH(id) AS id,
+    CHAR_LENGTH(name) AS name,
+    CHAR_LENGTH(legs) AS legs,
+    CHAR_LENGTH(arms) AS arms,
+    CHAR_LENGTH(characteristics) AS characteristics
+FROM monsters;
