694acd9ff76bbfed68660ad6c4729e2f7804e78f
Iskamele <iskamele@gmail.com>
2023-05-17 22:21:44 +0300

    Improved method.

--- src/main/java/Other/DirectoryNameConverterV2/service/impl/DirectoryCreatorServiceImpl.java
+++ src/main/java/Other/DirectoryNameConverterV2/service/impl/DirectoryCreatorServiceImpl.java
@@ -1,14 +1,16 @@
 package Other.DirectoryNameConverterV2.service.impl;
 
 import java.io.File;
-import java.util.List;
 import Other.DirectoryNameConverterV2.service.DirectoryCreatorService;
+import java.util.List;
 
 public class DirectoryCreatorServiceImpl implements DirectoryCreatorService {
     @Override
     public void createDirectories(String toPath, List<String> paths) {
+        new File(toPath + "/theory").mkdir();
+        new File(toPath + "/practice").mkdir();
         for (String path : paths) {
-            File directory = new File(toPath + "/" +  path);
+            File directory = new File(toPath + "/theory/"  +  path);
             directory.mkdir();
         }
     }
