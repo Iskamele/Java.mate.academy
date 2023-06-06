e50d312b74b7d1b3e4e82acf9eef152ab8cfad7d
Iskamele <iskamele@gmail.com>
2023-06-06 20:28:35 +0300

    Removed login method.

--- src/main/java/section09_Spring/topic06_SpringSecurityPart1/practice/SpringSecurity/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/section09_Spring/topic06_SpringSecurityPart1/practice/SpringSecurity/service/impl/AuthenticationServiceImpl.java
@@ -1,13 +1,10 @@
 package section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.service.impl;
 
-import java.util.Optional;
 import org.springframework.stereotype.Service;
-import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.exception.AuthenticationException;
 import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.model.User;
 import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.service.AuthenticationService;
 import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.service.ShoppingCartService;
 import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.service.UserService;
-import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.util.HashUtil;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -18,16 +15,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                                      ShoppingCartService shoppingCartService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
-    }
-
-    @Override
-    public User login(String email, String password) throws AuthenticationException {
-        Optional<User> user = userService.findByEmail(email);
-        if (user.isPresent() && user.get().getPassword().equals(
-                HashUtil.generateSecurePassword(password, user.get().getSalt()))) {
-            return user.get();
-        }
-        throw new AuthenticationException("Invalid login or password");
     }
 
     @Override
