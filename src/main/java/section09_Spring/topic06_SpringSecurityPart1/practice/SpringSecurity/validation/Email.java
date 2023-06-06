1ae29bd21fb997c0079b89ba141d8e76f56ebe0f
Iskamele <iskamele@gmail.com>
2023-06-06 20:28:50 +0300

    Implemented @Email annotation.

---
+++ src/main/java/section09_Spring/topic06_SpringSecurityPart1/practice/SpringSecurity/validation/Email.java
@@ -0,0 +1,20 @@
+package section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
+
+@Constraint(validatedBy = EmailValidator.class)
+@Target({ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Email {
+    String message() default "Invalid email";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
