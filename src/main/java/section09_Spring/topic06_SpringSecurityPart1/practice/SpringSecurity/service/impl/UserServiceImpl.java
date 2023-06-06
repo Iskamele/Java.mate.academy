0466de3a804f925ef427812e138351b87b965660
Iskamele <iskamele@gmail.com>
2023-06-06 20:28:40 +0300

    Removed salt and HashUtil. Added PasswordEncoder.

--- src/main/java/section09_Spring/topic06_SpringSecurityPart1/practice/SpringSecurity/service/impl/UserServiceImpl.java
+++ src/main/java/section09_Spring/topic06_SpringSecurityPart1/practice/SpringSecurity/service/impl/UserServiceImpl.java
@@ -1,28 +1,27 @@
 package section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.service.impl;
 
 import java.util.Optional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.dao.UserDao;
 import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.model.User;
 import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.service.UserService;
-import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.util.HashUtil;
 
 
 @Service
 public class UserServiceImpl implements UserService {
-    private static final int SALT_LENGTH = 10;
     private final UserDao userDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserDao userDao) {
+    public UserServiceImpl(UserDao userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User add(User user) {
-        String salt = HashUtil.getSalt(SALT_LENGTH);
-        String securePassword = HashUtil.generateSecurePassword(user.getPassword(), salt);
+        String securePassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(securePassword);
-        user.setSalt(salt);
         return userDao.add(user);
     }
 
