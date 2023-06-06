34845778f13ffab8695369146e4a674de1be3f2c
Iskamele <iskamele@gmail.com>
2023-06-06 20:28:21 +0300

    Added repeatPassword field and overrided equals method.

--- src/main/java/section09_Spring/topic06_SpringSecurityPart1/practice/SpringSecurity/dto/request/UserRequestDto.java
+++ src/main/java/section09_Spring/topic06_SpringSecurityPart1/practice/SpringSecurity/dto/request/UserRequestDto.java
@@ -1,8 +1,15 @@
 package section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.dto.request;
 
+import java.util.Objects;
+import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.validation.Email;
+import section09_Spring.topic06_SpringSecurityPart1.practice.SpringSecurity.validation.Password;
+
+@Password
 public class UserRequestDto {
+    @Email
     private String email;
     private String password;
+    private String repeatPassword;
 
     public String getEmail() {
         return email;
@@ -10,5 +17,23 @@ public class UserRequestDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserRequestDto that = (UserRequestDto) o;
+        return Objects.equals(email, that.email)
+                && Objects.equals(password, that.password)
+                && Objects.equals(repeatPassword, that.repeatPassword);
     }
 }
