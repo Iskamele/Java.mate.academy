6b5b2cc65763fbfc09b67a59e485a8ff5a456f59
Iskamele <iskamele@gmail.com>
2023-06-07 18:29:44 +0300

    Implemented Role model and service.

--- src/main/java/section09_Spring/topic07_SpringSecurityPart2/practice/SpringSecurityPartTwo/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/section09_Spring/topic07_SpringSecurityPart2/practice/SpringSecurityPartTwo/service/impl/AuthenticationServiceImpl.java
@@ -1,20 +1,27 @@
 package section09_Spring.topic07_SpringSecurityPart2.practice.SpringSecurityPartTwo.service.impl;
 
+import java.util.Set;
 import org.springframework.stereotype.Service;
+import section09_Spring.topic07_SpringSecurityPart2.practice.SpringSecurityPartTwo.model.Role;
 import section09_Spring.topic07_SpringSecurityPart2.practice.SpringSecurityPartTwo.model.User;
 import section09_Spring.topic07_SpringSecurityPart2.practice.SpringSecurityPartTwo.service.AuthenticationService;
+import section09_Spring.topic07_SpringSecurityPart2.practice.SpringSecurityPartTwo.service.RoleService;
 import section09_Spring.topic07_SpringSecurityPart2.practice.SpringSecurityPartTwo.service.ShoppingCartService;
 import section09_Spring.topic07_SpringSecurityPart2.practice.SpringSecurityPartTwo.service.UserService;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private final static String ROLE_USER_NAME = Role.RoleName.USER.name();
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
+    private final RoleService roleService;
 
     public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService shoppingCartService) {
+                                     ShoppingCartService shoppingCartService,
+                                     RoleService roleService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
     }
 
     @Override
@@ -22,6 +29,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
+        user.setRoles(Set.of(roleService.getByName(ROLE_USER_NAME)));
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
