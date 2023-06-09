041db8d80899a9d7eee7eece0ed45aaacb3da0e6
Iskamele <iskamele@gmail.com>
2023-06-09 09:45:18 +0300

    Implemented `"/login"` endpoint.

--- src/main/java/section09_Spring/topic09_SpringSecurityPart3/practice/SpringStateless/controller/AuthenticationController.java
+++ src/main/java/section09_Spring/topic09_SpringSecurityPart3/practice/SpringStateless/controller/AuthenticationController.java
@@ -1,10 +1,17 @@
 package section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.controller;
 
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.exception.AuthenticationException;
+import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.jwt.JwtTokenProvider;
 import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.model.User;
+import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.model.dto.UserLoginDto;
 import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.model.dto.UserRegistrationDto;
 import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.model.dto.UserResponseDto;
 import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.security.AuthenticationService;
@@ -14,11 +21,14 @@ import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.ser
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final UserMapper userMapper;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public AuthenticationController(AuthenticationService authenticationService,
-                                    UserMapper userMapper) {
+                                    UserMapper userMapper,
+                                    JwtTokenProvider jwtTokenProvider) {
         this.authenticationService = authenticationService;
         this.userMapper = userMapper;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/register")
@@ -26,5 +36,16 @@ public class AuthenticationController {
         User user = authenticationService.register(userRequestDto.getEmail(),
                 userRequestDto.getPassword());
         return userMapper.mapToDto(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody @Valid UserLoginDto userLoginDto)
+            throws AuthenticationException {
+        User user = authenticationService
+                .login(userLoginDto.getLogin(), userLoginDto.getPassword());
+        String token = jwtTokenProvider.createToken(user.getEmail(), user.getRoles().stream()
+                .map(role -> role.getRoleName().name())
+                .collect(Collectors.toList()));
+        return new ResponseEntity<>(Map.of("token", token), HttpStatus.OK);
     }
 }
