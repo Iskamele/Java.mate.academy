2b84c3585edc272cf38d3930d7b50be941e3364e
Iskamele <iskamele@gmail.com>
2023-06-09 09:41:19 +0300

    Implement configure method. Added permissions access.

--- src/main/java/section09_Spring/topic09_SpringSecurityPart3/practice/SpringStateless/config/SecurityConfig.java
+++ src/main/java/section09_Spring/topic09_SpringSecurityPart3/practice/SpringStateless/config/SecurityConfig.java
@@ -1,25 +1,54 @@
 package section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.jwt.JwtTokenProvider;
+import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.model.Role;
+import section09_Spring.topic09_SpringSecurityPart3.practice.SpringStateless.jwt.JwtConfigurer;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String ROLE_ADMIN = Role.RoleName.ADMIN.name();
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public SecurityConfig(UserDetailsService userDetailsService,
-                          PasswordEncoder passwordEncoder) {
+                          PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider) {
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/register", "/login", "inject").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/users/{id}")
+                .hasAuthority(ROLE_ADMIN)
+                .antMatchers(HttpMethod.DELETE, "/products/{id}")
+                .hasAuthority(ROLE_ADMIN)
+                .anyRequest()
+                .authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider))
+                .and()
+                .headers().frameOptions().disable();
     }
 }
